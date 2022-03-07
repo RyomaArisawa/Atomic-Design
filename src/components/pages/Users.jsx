b6cf81da52aac673e2aa99203e5145c571e91ada
RyomaArisawa <85384295+RyomaArisawa@users.noreply.github.com>
2022-03-07 14:18:41 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+import { useRecoilState } from "recoil";
+import { userState } from "../../store/userState";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `ryoma${val}`,
+    image: "https://source.unsplash.com/Sg3XwuEpybU",
+    email: "xxx.com",
+    phone: "xxx-xxx-xxx",
+    company: {
+      name: "xxx会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
+  return (
+    <SContainer>
+      <SUserArea>
+        <h2>Usersページです</h2>
+        <SearchInput />
+        <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
